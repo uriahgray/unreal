5d6cc44ea33c8ce0b53a21ffefb1695dda75be7b
Uriah Gray <u@u-p.co>
2023-11-11 12:54:46 +1100

    vue router

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 import { createProvider } from './vue-apollo'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   apolloProvider: createProvider(),
+  router,
   render: h => h(App)
 }).$mount('#app')
